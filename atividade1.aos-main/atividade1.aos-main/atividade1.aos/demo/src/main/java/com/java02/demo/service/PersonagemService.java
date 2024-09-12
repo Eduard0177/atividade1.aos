bb54e1a79e08561b9b1f7f2939eb51f27cc489eb
Eduard0177 <131684798+Eduard0177@users.noreply.github.com>
2024-09-12 10:58:52 -0300

    Update PersonagemService.java

--- atividade1.aos-main/atividade1.aos-main/atividade1.aos/demo/src/main/java/com/java02/demo/service/PersonagemService.java
+++ atividade1.aos-main/atividade1.aos-main/atividade1.aos/demo/src/main/java/com/java02/demo/service/PersonagemService.java
@@ -1,6 +1,7 @@
 package com.java02.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,27 @@ public class PersonagemService {
         return personagemRepository.findByRaca(raca);
     }
 
+    public void deleteById(Long id) {
+        if (personagemRepository.existsById(id)) {
+            personagemRepository.deleteById(id);
+        }
+    }
+
+    public Personagem updatePersonagemById(Long id, Personagem personagemAtualizado) {
+        Optional<Personagem> personagemExistente = personagemRepository.findById(id);
+        
+        if (personagemExistente.isPresent()) {
+            Personagem personagem = personagemExistente.get();
+
+            // Atualizando os campos do personagem com base nos dados fornecidos
+            personagem.setNome(personagemAtualizado.getNome());
+            personagem.setRaca(personagemAtualizado.getRaca());
     
+            // Adicione aqui quaisquer outros campos que precisam ser atualizados
+
+            // Salvando o personagem atualizado no banco de dados
+            return personagemRepository.save(personagem);
+        }
+    
+}
 }
